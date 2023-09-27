08ef03275971cf759e9c1e4bcac21bffae510eac
Julian Simón <juedsay@gmail.com>
2023-09-27 00:05:27 -0300

    modificación en la clase tiposDeEspera con comentarios y tipos de Timeouts de ejemplos

--- __tests__/tiposDeEspera.js
+++ __tests__/tiposDeEspera.js
@@ -2,13 +2,24 @@ const puppeteer = require('puppeteer')
 
 describe('Tipos de Espera',()=> { //Función que recibe un callback
 
-    it('Mostrar todos los diferentes tipos de espera', async() =>{ //Es una función asincrona porque necesito esperar a que se abra el navegador
+        //<<<<<<<<<<<<<<------------------------------------------------->>>>>>>>>>>>>>>//
+        //Configuración del Timeout:
+        jest.setTimeout(10000)
+        //-------------------------------------------------//
+
+    it('Mostrar todos los diferentes tipos de espera', async() =>{ //Es una función asincrona porque necesito esperar a que se abra el navegador       
+
         const browser = await puppeteer.launch({
             headless: false, //Si está en falso SI se puede ver el navegador, si está en verdadero NO se puede ver el Navegador
             defaultViewport: null,//Hace que la web tome el tamaño de la ventana
             //slowMo: 500
         })
         const page = await browser.newPage()
+        //<<<<<<<<<<<<<<------------------------------------------------->>>>>>>>>>>>>>>//
+        //Configuración de otros tipo de Timeout:
+        page.setDefaultTimeout(10000)
+        page.setDefaultNavigationTimeout(10000)//Es para cuando esté navegando entre páginas qué tanto debo esperar para a que la navegación termine
+        //<<<<<<<<<<<<<<------------------------------------------------->>>>>>>>>>>>>>>//
         //Espera que termine de cargar el logo de la pestaña
         // await page.goto('https://www.github.com', {waitUntil : 'networkidle0'})
 
@@ -45,10 +56,11 @@ describe('Tipos de Espera',()=> { //Función que recibe un callback
         //Para cerrar el Modal:
         await page.click('#closeSmallModal')
         //Ahora lo niego, o sea me tiene que regresar un False, la modificación es el "!" delante de document
-        await page.waitForFunction(()=> !document.querySelector('#example-modal-sizes-title-sm').innerText === 'Small Modal')
+        await page.waitForFunction(()=> !document.querySelector('#example-modal-sizes-title-sm'), 
+        { timeout: 30000} )//<<<<<AQUI APLICO OTRO TIPO DE TIMEOUT >>>>//
 
         await browser.close()    
         
-    }, 35000)
+    }, 35000)//<<<<<<<<<<<<<<--ESTE ES OTRO TIPO DE TIMEOUT MAS COMODO DE CONFIGURAR--->>>>>>>>>>>>>>>//
 
 })
